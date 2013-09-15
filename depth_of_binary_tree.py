96b4f40ede6b0523cb51adb7bec4586b26a5980c
Seb Insua <me@sebinsua.com>
2013-09-15 14:11:55 +0100

    Check if tree is balanced or not.

--- depth_of_binary_tree.py
+++ depth_of_binary_tree.py
@@ -2,6 +2,16 @@ class BinaryTreeNode:
     left_node = None
     right_node = None
 
+    def is_balanced(self):
+        if self.left_node is None and self.right_node is None:
+            return True
+
+        left_height = get_depth(self.left_node)
+        right_height = get_depth(self.right_node)
+
+        return abs(left_height - right_height) <= 1
+
+
 def get_depth(binary_tree_node, depth = 0):
     if binary_tree_node:
         depth += 1
@@ -29,6 +39,14 @@ def build_tree(tree_length):
     elif tree_length == 3:
         binary_tree_nodeB.right_node = binary_tree_nodeC
         binary_tree_nodeA.left_node = binary_tree_nodeB
+    elif tree_length == 'balanced':
+        binary_tree_nodeA.left_node = binary_tree_nodeB
+        binary_tree_nodeA.right_node = binary_tree_nodeC
+    elif tree_length == 'unbalanced':
+        binary_tree_nodeD.left_node = binary_tree_nodeE
+        binary_tree_nodeB.left_node = binary_tree_nodeD
+        binary_tree_nodeA.left_node = binary_tree_nodeB
+        binary_tree_nodeA.right_node = binary_tree_nodeC
     else:
         binary_tree_nodeH.left_node = binary_tree_nodeI
         binary_tree_nodeG.left_node = binary_tree_nodeH
@@ -47,3 +65,7 @@ if __name__ == "__main__":
     print get_depth(build_tree(5)) == 5
     print get_depth(build_tree(3)) == 3
     print get_depth(build_tree("?")) == 9
+    print get_depth(build_tree("balanced")) == 2
+
+    print build_tree('balanced').is_balanced()
+    print build_tree('unbalanced').is_balanced()
